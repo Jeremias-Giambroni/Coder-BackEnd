07e97ae27e83bc2ec7cfdb8874493d299e895c0d
Jeremias <jeremiasgiambroni@gmail.com>
2025-12-09 16:47:41 -0300

    Clase 0

---
+++ 01.Clase/01.App.js
@@ -0,0 +1,6 @@
+console.log("Hola coders");
+
+console.warn("Alerta");
+
+console.error("Error");
+
---
+++ 01.Clase/02.App.js
@@ -0,0 +1,27 @@
+let nombre = "Juan Pablo"
+console.log(nombre);
+
+console.log("--------------");
+
+nombre = "Pedro"
+console.log(nombre);
+
+
+let array = ["Pedro", "Luis", "Felipe"];
+
+console.log(array);
+
+let edad = 44;
+
+console.log(edad);
+ 
+//dos maneras de sumarle 1 a la edad
+edad = edad + 1
+edad ++
+
+console.log(edad);
+
+
+
+
+
